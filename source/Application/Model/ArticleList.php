53f4527e1e1cd3c401594130c900cc754f699505
Gregor Hyneck <gregor.hyneck@oxid-esales.com>
2016-05-24 13:45:31 +0200

    ESDEV-3670 delete more occurences of oxLegacyDb

--- source/Application/Model/ArticleList.php
+++ source/Application/Model/ArticleList.php
@@ -1286,7 +1286,7 @@ class ArticleList extends \oxList
      * Updates article.
      *
      * @param string     $sCurrUpdateTime
-     * @param oxLegacyDb $oDb
+     * @param DatabaseInterface $oDb
      *
      * @return mixed
      */
